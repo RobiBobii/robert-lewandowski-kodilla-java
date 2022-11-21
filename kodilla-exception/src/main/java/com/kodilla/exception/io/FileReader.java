0ab2186287fe43aca7399c1ab683bb45b9f09960
Robert Lewandowski <rlewandowski232@gmail.com>
2022-11-21 13:06:35 +0100

    8.3 Second Challenge

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -3,22 +3,28 @@ package com.kodilla.exception.io;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 public class FileReader {
 
-    public void readFile() {
-        ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(classLoader.getResource("names.txt").getFile());
+    public void readFile() throws FileReaderException{
 
-        try (Stream<String> fileLines = Files.lines(Paths.get("tego-pliku-nie-ma.txt"))) {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("file/names.txt").getFile());
+        System.out.println(file.getPath());
+
+        try (Stream<String> fileLines = Files.lines(Paths.get(file.getPath()))) {
             fileLines.forEach(System.out::println);
         } catch (IOException e) {
-            System.out.println("Oh no! Something went wrong! Error:" + e);
+            throw new FileReaderException();
+            //System.out.println("something is wrong! Error: " + e);
         } finally {
-            System.out.println("I am gonna be here... always!");
+            System.out.println("I am gonna be here.. always!");
+
         }
+
     }
+
+
 }
