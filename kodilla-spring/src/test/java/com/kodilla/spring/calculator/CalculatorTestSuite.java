69109dceeb5797c4f18efc645b88cad1be427615
Robert Lewandowski <rlewandowski232@gmail.com>
2023-01-06 23:31:37 +0100

    14.2 Calculator

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -0,0 +1,48 @@
+package com.kodilla.spring.calculator;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CalculatorTestSuite {
+
+
+    @Test
+    public void testContext() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+
+        //When & Then
+        System.out.println("===== Beans list: ==== >>");
+        Arrays.stream(context.getBeanDefinitionNames())
+                .forEach(System.out::println);
+        System.out.println("<< ===== Beans list ====");
+    }
+
+    @Test
+    public void testCalculations() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        double a = 10;
+        double b = 5;
+        //When
+        double addResult = calculator.add(a, b);
+        double subResult = calculator.sub(a, b);
+        double mulResult = calculator.mul(a, b);
+        double divResult = calculator.div(a, b);
+        //Then
+        Assert.assertEquals(15, addResult, 0);
+        Assert.assertEquals(5, subResult, 0);
+        Assert.assertEquals(50, mulResult, 0);
+        Assert.assertEquals(2, divResult, 0);
+    }
+}
