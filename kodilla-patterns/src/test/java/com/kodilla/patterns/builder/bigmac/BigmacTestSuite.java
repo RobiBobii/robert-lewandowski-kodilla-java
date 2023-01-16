dc4d874c1cf43c8b5a1805d051693c2c2c6aca6e
Robert Lewandowski <rlewandowski232@gmail.com>
2023-01-16 15:19:48 +0100

    15.5 BigMac

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -0,0 +1,34 @@
+package com.kodilla.patterns.builder.bigmac;
+
+import org.junit.jupiter.api.Test;
+import org.testng.Assert;
+
+
+public class BigmacTestSuite {
+
+    @Test
+    public void testBigMacBuilder() {
+        //given
+        Bigmac bigmac = new Bigmac.BigmacBuilder()
+                .bun("black")
+                .ingredient("cheese")
+                .ingredient("salad")
+                .ingredient("onion")
+                .ingredient("pickles")
+                .sauce("sweet and sour")
+                .burgers(2)
+                .build();
+
+        System.out.println(bigmac);
+        //when
+        int countBurgers = bigmac.getBurgers();
+        int countIngredients = bigmac.getIngredients().size();
+        String bunType = bigmac.getBun();
+        //then
+        Assert.assertEquals(2,countBurgers);
+        Assert.assertEquals(4,countIngredients);
+        Assert.assertEquals("black",bunType);
+        Assert.assertNotNull(bigmac.getSauce());
+
+    }
+}
